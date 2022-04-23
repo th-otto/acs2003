5b48f929363d00679de4ba4af2a4a3e66f652bd0
Thorsten Otto <admin@tho-otto.de>
2022-04-23 11:44:45 +0200

    Update mkzip script

--- src/mkzip.sh
+++ src/mkzip.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-for i in `find . \( -name "*.s" -o -name "*.c" -o -name "*.h" -o -name "*.prj" \)`; do cp -a "$i" ../2003/src/$i; done
-(cd ..; zip -r 2003.zip 2003)
+git archive --format=zip -o ../2003.zip --prefix=2003/ HEAD
