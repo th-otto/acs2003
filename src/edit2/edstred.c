df371e5e795f5e1e6168429c1f9fc85d21704c47
Thorsten Otto <admin@tho-otto.de>
2022-05-19 12:50:10 +0200

    Replace edstred.s by C version

--- src/edit2/edstred.c
+++ src/edit2/edstred.c
@@ -10,8 +10,162 @@ static boolean st_service(Awindow *self, int16 task, void *in_out);
 static Awindow *st_make(void *a);
 
 #include "edstred.ah"
+#include "edstred.h"
+
+static void editstr_term(Awindow *self);
+static void acc_string(Awindow *self);
+static void set_string(Awindow *self);
 
 /******************************************************************************/
 /* -------------------------------------------------------------------------- */
 /******************************************************************************/
 
+static void editstr_abort(void)
+{
+	editstr_term(ACSblk->ev_window);
+}
+
+/* -------------------------------------------------------------------------- */
+
+static void editstr_ok(void)
+{
+	acc_string(ACSblk->ev_window);
+	editstr_term(ACSblk->ev_window);
+}
+
+/* -------------------------------------------------------------------------- */
+
+STATIC void acc_string(Awindow *self)
+{
+	EDPARM *parm;
+	char buf[512];
+	char *str2;
+	char *str1;
+	OBJECT *tree;
+	char *newstr;
+	Awindow *win;
+	
+	parm = self->user;
+	parm->acs->flags |= ACS_CHANGED;
+	tree = self->work;
+	Auo_boxed(&tree[EDIT_STRING_STR1], AUO_GETVAL, &str1);
+	Auo_boxed(&tree[EDIT_STRING_STR2], AUO_GETVAL, &str2);
+	strcat(strcpy(buf, str1), str2);
+	if ((newstr = Ast_create(buf)) != NULL)
+	{
+		Ax_free(parm->obj->object);
+		parm->obj->object = newstr;
+		parm->obj->used = parm->obj->size = strlen(newstr) + 1;
+		win = parm->acs->st_list->window;
+		if (win != NULL)
+			win->state |= AWS_LATEUPDATE;
+		(void)tree; /* FIXME: useless cast to get registers right */
+	}
+}
+/* -------------------------------------------------------------------------- */
+
+static Awindow *st_make(void *a)
+{
+	EDPARM *parm;
+	EDPARM *newparm;
+	Obj_Head *obj;
+	Awindow *win;
+	
+	parm = a;
+	obj = parm->obj;
+	win = obj->window;
+	if (win != NULL)
+	{
+		Awi_show(win);
+	} else
+	{
+		if ((newparm = Ax_malloc(sizeof(*newparm))) == NULL)
+			return NULL;
+		newparm->acs = parm->acs;
+		newparm->obj = parm->obj;
+		WI_STRING.iconblk->monoblk.ib_ptext = WI_STRING.info = obj->label;
+		win = Awi_create(&WI_STRING);
+		if (win == NULL)
+		{
+			/* Ax_free(newparm); BUG: missing */
+			return NULL;
+		}
+		wi_pos(win, &obj->pos, &parm->acs->last_rez);
+		win->user = newparm;
+		obj->window = win;
+		set_string(win);
+		if (win->open(win) != OK)
+		{
+			editstr_term(win);
+			return NULL;
+		}
+	}
+	return win;
+}
+
+/* -------------------------------------------------------------------------- */
+
+static boolean st_service(Awindow *self, int16 task, void *in_out)
+{
+	EDPARM *parm = self->user;
+	
+	switch (task)
+	{
+	case AS_TERM:
+		acc_string(self);
+		editstr_term(self);
+		break;
+	
+	case AS_GUI_RENAME_OBJ:
+		new_name(self, parm->obj);
+		break;
+	
+	default:
+		return Awi_service(self, task, in_out);
+	}
+	return TRUE;
+}
+
+/* -------------------------------------------------------------------------- */
+
+static void set_string(Awindow *self)
+{
+	EDPARM *parm = self->user;
+	OBJECT *tree;
+	char buf[256];
+	int len;
+	char *str;
+	
+	tree = self->work;
+	str = parm->obj->object;
+	len = (int)strlen(str);
+	if (len > 255)
+	{
+		strncpy(buf, &str[255], 255);
+		buf[255] = '\0';
+		len = 255;
+	} else
+	{
+		buf[0] = '\0';
+	}
+	Aob_printf(tree, EDIT_STRING_STR1, "%-*s", len, str);
+	Aob_puttext(tree, EDIT_STRING_STR2, buf);
+}
+
+/* -------------------------------------------------------------------------- */
+
+static void editstr_term(Awindow *self)
+{
+	EDPARM *parm;
+	
+	self->state |= AWS_TERM;
+	parm = self->user;
+	parm->obj->window = NULL;
+	if (self->state & AWS_ICONIZED)
+		memcpy(&parm->obj->pos, &self->wi_normal, sizeof(parm->obj->pos));
+	else
+		memcpy(&parm->obj->pos, &self->wi_act, sizeof(parm->obj->pos));
+	Ax_free(parm);
+	Awi_closed(self);
+	Awi_delete(self);
+}
---
+++ src/edit2/edstred.h
@@ -0,0 +1,2 @@
+#define EDIT_STRING_STR1    3
+#define EDIT_STRING_STR2    4
