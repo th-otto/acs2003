2863f4cf0e8d2711e021031999c32e00ba3a776c
Thorsten Otto <admin@tho-otto.de>
2022-07-02 10:36:41 +0200

    Replace seproto.s by C version

---
+++ src/lib/messages/seproto.c
@@ -0,0 +1,25 @@
+/******************************************************************************/
+/*                                                                            */
+/*    ACS               Application Construction System                       */
+/*    DESCRIPTION:      SE protocol (NYI)                                     */
+/*                                                                            */
+/* (c) 1991-2004 Stefan Bachert, Oliver Michalak, Martin Elsaesser            */
+/* (c) 2022 Thorsten Otto                                                     */
+/******************************************************************************/
+
+#include "acs_i.h"
+#include "msgserv.h"
+
+/* -------------------------------------------------------------------------- */
+
+boolean Aev_InitSE(void)
+{
+	return TRUE;
+}
+
+/* -------------------------------------------------------------------------- */
+
+boolean Aev_ExitSE(void)
+{
+	return TRUE;
+}
--- src/lib/messages/seproto.s
+++
@@ -1,9 +0,0 @@
-	.globl Aev_InitSE
-Aev_InitSE:
-		moveq.l    #1,d0
-		rts
-
-	.globl Aev_ExitSE
-Aev_ExitSE:
-		moveq.l    #1,d0
-		rts
