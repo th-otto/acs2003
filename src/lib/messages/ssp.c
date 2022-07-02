b500bba108e0d9d8b66a860c57a05e420ca535ed
Thorsten Otto <admin@tho-otto.de>
2022-07-02 10:39:53 +0200

    Replace ssp.s by C version

---
+++ src/lib/messages/ssp.c
@@ -0,0 +1,25 @@
+/******************************************************************************/
+/*                                                                            */
+/*    ACS               Application Construction System                       */
+/*    DESCRIPTION:      System Services Protocol (NYI)                        */
+/*                                                                            */
+/* (c) 1991-2004 Stefan Bachert, Oliver Michalak, Martin Elsaesser            */
+/* (c) 2022 Thorsten Otto                                                     */
+/******************************************************************************/
+
+#include "acs_i.h"
+#include "msgserv.h"
+
+/* -------------------------------------------------------------------------- */
+
+boolean Aev_InitSSP(void)
+{
+	return TRUE;
+}
+
+/* -------------------------------------------------------------------------- */
+
+boolean Aev_ExitSSP(void)
+{
+	return TRUE;
+}
--- src/lib/messages/ssp.s
+++
@@ -1,9 +0,0 @@
-	.globl Aev_InitSSP
-Aev_InitSSP:
-		moveq.l    #1,d0
-		rts
-
-	.globl Aev_ExitSSP
-Aev_ExitSSP:
-		moveq.l    #1,d0
-		rts
