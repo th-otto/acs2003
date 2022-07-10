2fac943cf39c53a1deb0fa2c2d4e1a2354502c16
Thorsten Otto <admin@tho-otto.de>
2022-07-10 15:24:35 +0200

    Add sources of ON3D/NO3D modules

---
+++ module/on3d/on3d.c
@@ -0,0 +1,21 @@
+#include	<stdio.h>
+#include	<string.h>
+#include	<acs.h>
+
+int16 ACSinit(void)
+{
+	int16 msg[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+	
+	ACSblk->description->flags &= ~AB_NO3D;
+	msg[0] = WM_REDRAW;
+	msg[1] = ACSblk->gl_apid;
+	msg[4] = ACSblk->desk.x;
+	msg[5] = ACSblk->desk.y;
+	msg[6] = ACSblk->desk.w;
+	msg[7] = ACSblk->desk.h;
+	appl_write(ACSblk->gl_apid, (int)sizeof(msg), msg);
+	form_dial(FMD_FINISH, ACSblk->desk.x, ACSblk->desk.y, ACSblk->desk.w, ACSblk->desk.h,
+		ACSblk->desk.x, ACSblk->desk.y, ACSblk->desk.w, ACSblk->desk.h);
+	ACSmoduleterm();
+	return OK;
+}
